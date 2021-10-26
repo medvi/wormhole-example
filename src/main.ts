3a515da588b99d7c359c3d4f31afdcd42cf3fb69
Leonid Chedrik <leonid.chedrik@algalon.com>
2021-10-26 23:54:27 +0300

    working

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as sweb3 from "@solana/web3.js";
 import { getProvider, PhantomProvider } from "./phantom";
-import detectEthereumProvider from '@metamask/detect-provider';
-import { attestFromSolana } from "@certusone/wormhole-sdk";
+import detectEthereumProvider from "@metamask/detect-provider";
+import * as wormhole from "@certusone/wormhole-sdk";
 import { ethers } from "ethers";
 
 const PHANTOM_PROVIDER: PhantomProvider | undefined = getProvider();
@@ -14,10 +14,14 @@ const METAMASK_WALLET_SPAN = document.getElementById("metamask_wallet_span");
 
 const SOL_AMOUNT_INPUT = document.getElementById("sol_amount_input");
 const ATTEST_SOL_TO_ETH_BTN = document.getElementById("attest_sol_to_eth_btn");
-const TRANSFER_SOL_TO_ETH_BTN = document.getElementById("transfer_sol_to_eth_btn");
+const TRANSFER_SOL_TO_ETH_BTN = document.getElementById(
+  "transfer_sol_to_eth_btn"
+);
 const ETH_AMOUNT_INPUT = document.getElementById("eth_amount_input");
 const ATTEST_ETH_TO_SOL_BTN = document.getElementById("attest_eth_to_sol_btn");
-const TRANSFER_ETH_TO_SOL_BTN = document.getElementById("transfer_eth_to_sol_btn");
+const TRANSFER_ETH_TO_SOL_BTN = document.getElementById(
+  "transfer_eth_to_sol_btn"
+);
 
 const SOL_BRIDGE_ADDRESS = "Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o";
 const SOL_TOKEN_BRIDGE_ADDRESS = "B6RHG3mfcckmrYN1UhmJzyS1XX3fZKbkeUcpJe9Sy3FE";
@@ -27,11 +31,11 @@ const ETH_TOKEN_BRIDGE_ADDRESS = "0x0290FB167208Af455bB137780163b7B7a9a10C16";
 const TOKEN_ADDRESS = "0x2D8BE6BF0baA74e0A907016679CaE9190e80dD0A";
 const WORMHOLE_RPC_HOST = "http://localhost:8080";
 
-const SIGNER = new ethers.Wallet('...');
+const SIGNER = new ethers.Wallet("...");
 
 async function attestSolToEth() {
   // Submit transaction - results in a Wormhole message being published
-  const transaction = await attestFromSolana(
+  const transaction = await wormhole.attestFromSolana(
     CONNECTION,
     SOL_BRIDGE_ADDRESS,
     SOL_TOKEN_BRIDGE_ADDRESS,
@@ -45,7 +49,9 @@ async function attestSolToEth() {
   // Get the sequence number and emitter address required to fetch the signedVAA of our message
   const info = await CONNECTION.getTransaction(txid);
   const sequence = wormhole.parseSequenceFromLogSolana(info);
-  const emitterAddress = await wormhole.getEmitterAddressSolana(SOL_TOKEN_BRIDGE_ADDRESS);
+  const emitterAddress = await wormhole.getEmitterAddressSolana(
+    SOL_TOKEN_BRIDGE_ADDRESS
+  );
   // Fetch the signedVAA from the Wormhole Network (this may require retries while you wait for confirmation)
   const { signedVAA } = await wormhole.getSignedVAA(
     WORMHOLE_RPC_HOST,
@@ -54,12 +60,14 @@ async function attestSolToEth() {
     sequence
   );
   // Create the wrapped token on Ethereum
-  await wormhole.createWrappedOnEth(ETH_TOKEN_BRIDGE_ADDRESS, SIGNER, signedVAA);
+  await wormhole.createWrappedOnEth(
+    ETH_TOKEN_BRIDGE_ADDRESS,
+    SIGNER,
+    signedVAA
+  );
 }
 
-async function transferSolToEth() {
-
-}
+async function transferSolToEth() {}
 
 async function attestEthToSol() {
   // Submit transaction - results in a Wormhole message being published
@@ -69,8 +77,13 @@ async function attestEthToSol() {
     TOKEN_ADDRESS
   );
   // Get the sequence number and emitter address required to fetch the signedVAA of our message
-  const sequence = wormhole.parseSequenceFromLogEth(receipt, ETH_BRIDGE_ADDRESS);
-  const emitterAddress = wormhole.getEmitterAddressEth(ETH_TOKEN_BRIDGE_ADDRESS);
+  const sequence = wormhole.parseSequenceFromLogEth(
+    receipt,
+    ETH_BRIDGE_ADDRESS
+  );
+  const emitterAddress = wormhole.getEmitterAddressEth(
+    ETH_TOKEN_BRIDGE_ADDRESS
+  );
   // Fetch the signedVAA from the Wormhole Network (this may require retries while you wait for confirmation)
   const { signedVAA } = await wormhole.getSignedVAA(
     WORMHOLE_RPC_HOST,
@@ -99,16 +112,14 @@ async function attestEthToSol() {
   await CONNECTION.confirmTransaction(txid);
 }
 
-async function transferEthToSol() {
-  
-}
+async function transferEthToSol() {}
 
 function events() {
-  ATTEST_SOL_TO_ETH_BTN?.addEventListener("click", attestSolToEth, false);;
-  TRANSFER_SOL_TO_ETH_BTN?.addEventListener("click", transferSolToEth, false);;
-  ATTEST_ETH_TO_SOL_BTN?.addEventListener("click", attestEthToSol, false);;
-  TRANSFER_ETH_TO_SOL_BTN?.addEventListener("click", transferEthToSol, false);;
-  
+  ATTEST_SOL_TO_ETH_BTN?.addEventListener("click", attestSolToEth, false);
+  TRANSFER_SOL_TO_ETH_BTN?.addEventListener("click", transferSolToEth, false);
+  ATTEST_ETH_TO_SOL_BTN?.addEventListener("click", attestEthToSol, false);
+  TRANSFER_ETH_TO_SOL_BTN?.addEventListener("click", transferEthToSol, false);
+
   if (PHANTOM_PROVIDER !== undefined) {
     if (!PHANTOM_PROVIDER.isConnected) {
       PHANTOM_PROVIDER.connect().then(async () => {
