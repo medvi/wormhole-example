23efcf8163585bb11454aff940e23803527cf948
Leonid Chedrik <leonid.chedrik@algalon.com>
2021-10-26 23:47:50 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [],
+  resolve: {
+    alias: [
+      {
+        find: "stream",
+        replacement: "stream-browserify",
+      },
+      {
+        find: "process",
+        replacement: "process/browser",
+      },
+      {
+        find: "zlib",
+        replacement: "browserify-zlib",
+      },
+      {
+        find: "util",
+        replacement: "util",
+      },
+    ],
+  },
   define: {
     global: {},
+  },
+  build: {
+    rollupOptions: {
+      plugins: [],
+    },
   },
 });
